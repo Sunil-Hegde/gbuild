dea872e97e14298804d22d31c27d766e5778cb80
Sunil-Hegde <sunil1hegde@gmail.com>
2024-04-03 00:01:21 +0530

    firebase auth

---
+++ firebase/firebaseLogin.js
@@ -0,0 +1,55 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.1/firebase-app.js";
+import { 
+    getDatabase, 
+    ref, 
+    update 
+} from "https://www.gstatic.com/firebasejs/10.5.1/firebase-database.js";
+import { 
+    getAuth, 
+    GoogleAuthProvider,
+    signInWithPopup 
+} from "https://www.gstatic.com/firebasejs/10.5.1/firebase-auth.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyADJQq2fz6b6VXZnAz4vjnC9td11o5m4xY",
+    authDomain: "gbuild-d2700.firebaseapp.com",
+    projectId: "gbuild-d2700",
+    storageBucket: "gbuild-d2700.appspot.com",
+    messagingSenderId: "536218718918",
+    appId: "1:536218718918:web:fa22f6fda6678644333dc7",
+    measurementId: "G-GWV0KG2TW6"
+  };
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+document.addEventListener('DOMContentLoaded', function () {
+    const signInWithGoogleButton = document.querySelector('.signInButton.signInWithGoogleButton');
+    signInWithGoogleButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            const info = result.user;
+            var email = info.email;
+            var username = info.displayName;
+            const userId = result.user.uid;
+            const userRef = ref(database, 'users/' + userId);
+            const dt = new Date();
+            update(userRef, {
+                username: username,
+                email: email,
+                last_login: dt,
+            })
+            .then(() => {
+                window.location.href = 'index.html';
+            })
+            .catch((error) => {
+                console.error('Error during sign-in:', error.code, error.message);
+            });
+        })
+        .catch((error) => {
+            console.error('Error during sign-in:', error.code, error.message);
+        });
+    });
+});
