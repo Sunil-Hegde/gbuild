770f927f668fededbb6ee10528d29820d3d5e304
Sunil-Hegde <sunil1hegde@gmail.com>
2024-04-03 00:55:12 +0530

    firebase auth

--- firebase/firebaseLogin.js
+++ firebase/firebaseLogin.js
@@ -2,13 +2,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.1/firebas
 import { 
     getDatabase, 
     ref, 
-    update 
+    update,
+    get
 } from "https://www.gstatic.com/firebasejs/10.5.1/firebase-database.js";
 import { 
     getAuth,  
     GoogleAuthProvider,
-    signInWithPopup 
+    signInWithPopup, 
+    onAuthStateChanged, 
+    signOut 
 } from "https://www.gstatic.com/firebasejs/10.5.1/firebase-auth.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyADJQq2fz6b6VXZnAz4vjnC9td11o5m4xY",
     authDomain: "gbuild-d2700.firebaseapp.com",
@@ -18,42 +22,56 @@ const firebaseConfig = {
     messagingSenderId: "536218718918",
     appId: "1:536218718918:web:fa22f6fda6678644333dc7",
     measurementId: "G-GWV0KG2TW6"
-  };
+};
+
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+
 document.addEventListener('DOMContentLoaded', function () {
     const signInWithGoogleButton = document.querySelector('.signInButton.signInWithGoogleButton');
-    signInWithGoogleButton.addEventListener('click', (e) => {
-        console.log("Hello");
-        e.preventDefault();
-        signInWithPopup(auth, provider)
-        .then((result) => {
-            console.log("Hello");
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            const info = result.user;
-            var email = info.email;
-            var username = info.displayName;
-            const userId = result.user.uid;
+    const logoutButton = document.getElementById('logout');
+    const trialElement = document.getElementById('username');
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            signInWithGoogleButton.style.display = 'none';
+            logoutButton.style.display = 'block';
+            const userId = user.uid;
             const userRef = ref(database, 'users/' + userId);
-            const dt = new Date();
-            update(userRef, {
-                username: username,
-                email: email,
-                last_login: dt,
-            })
-            .then(() => {
-                window.location.href = 'index.html';
-                console.log("logged in");
+            get(userRef)
+            .then((userData) => {
+                if (userData.exists()) {
+                    const username = userData.val().username;
+                    const userEmail = userData.val().email;
+                    trialElement.innerHTML = `<h2>Hello ${username}</h2>`;
+                }
             })
             .catch((error) => {
-                console.error('Error during sign-in:', error.code, error.message);
+                console.error("Error checking user data:", error);
             });
-        })
+        } else {
+            signInWithGoogleButton.style.display = 'block';
+            logoutButton.style.display = 'none';
+            trialElement.innerHTML = '<h2>Please login</h2>';
+        }
+    });
+
+    signInWithGoogleButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        signInWithPopup(auth, provider)
         .catch((error) => {
             console.error('Error during sign-in:', error.code, error.message);
+        });
+    });
+
+    logoutButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        signOut(auth)
+        .catch((error) => {
+            console.error("Error during sign-out:", error);
+            alert('Error during sign-out.');
         });
     });
 });
